7b99b1ee6be0b6c652dc234b5a277fb0f4f454b0
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-10-21 16:55:01 -0300

    Improving Interface

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -15,76 +15,80 @@ class _TodoListPageState extends State<TodoListPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: todoController,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Adicione uma tarefa',
-                        hintText: 'Ex: Estudar Flutter',
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          todos.add(todoController.text);
-                        });
-                        todoController.clear();
-                      },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(const Color(0xff00d7f3)),
-                      padding: MaterialStateProperty.all(const EdgeInsets.all(14))
-                    ),
-                      child: const Icon(
-                        Icons.add,
-                        size: 30,
-                        color: Colors.white,
-                      ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 16),
-              Flexible(
-                child: ListView(
-                  shrinkWrap: true,
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
                   children: [
-                      for(String todo in todos)
-                        const TodoListItem()
+                    Expanded(
+                      child: TextField(
+                        controller: todoController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Adicione uma tarefa',
+                          hintText: 'Ex: Estudar Flutter',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            todos.add(todoController.text);
+                          });
+                          todoController.clear();
+                        },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(const Color(0xff00d7f3)),
+                        padding: MaterialStateProperty.all(const EdgeInsets.all(14))
+                      ),
+                        child: const Icon(
+                          Icons.add,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                    )
                   ],
                 ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  const Expanded(
-                      child: Text('Você possui 0 tarefas pendentes')
+                const SizedBox(height: 16),
+                Flexible(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                        for(String todo in todos)
+                          TodoListItem(
+                            title: todo,
+                          )
+                    ],
                   ),
-                  const SizedBox(width: 8),
-                  ElevatedButton(
-                      onPressed: null,
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(const Color(0xff00d7f3)),
-                          padding: MaterialStateProperty.all(const EdgeInsets.all(14)),
-                      ),
-                      child: const Text(
-                          'Limpar tudo',
-                        style: TextStyle(color: Colors.white),
-                      )
-                  )
-                ],
-              )
-            ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text('Você possui ${todos.length} tarefas pendentes')
+                    ),
+                    const SizedBox(width: 8),
+                    ElevatedButton(
+                        onPressed: null,
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(const Color(0xff00d7f3)),
+                            padding: MaterialStateProperty.all(const EdgeInsets.all(14)),
+                        ),
+                        child: const Text(
+                            'Limpar tudo',
+                          style: TextStyle(color: Colors.white),
+                        )
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem ({Key? key}) : super(key: key);
+  const TodoListItem ({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(4),
-        color: Colors.grey[200],
+        color: Colors.grey[200]
       ),
-      padding: const EdgeInsets.all(8),
-      child: const Column(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(vertical: 2),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
               '10/10/2010',
             style: TextStyle(
               fontSize: 12,
             ),
           ),
           Text(
-              'Trefa',
-            style: TextStyle(
+            title,
+            style: const TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.w600
             ),
